bb11e3a3a9ccffd592dcb85f4eff58934c3d32b8
Pavel Ershov <devnulpavel@gmail.com>
2019-04-26 16:08:36 +0300

    Additional commentaries

--- OpenCL_Books_CodeTest/RavishekharBanger/Code/chapter-3/copyRectangular.cpp
+++ OpenCL_Books_CodeTest/RavishekharBanger/Code/chapter-3/copyRectangular.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #ifdef __APPLE__
-#include <OpenCL/cl.h>
+    #include <OpenCL/cl.h>
 #else
-#include <CL/cl.h>
+    #include <CL/cl.h>
 #endif
 #include <ocl_macros.h>
 #include <iostream>
@@ -10,66 +10,49 @@
 #define NUM_OF_ELEMENTS 32 
 #define DEVICE_TYPE CL_DEVICE_TYPE_GPU
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     cl_int           status = 0;
     cl_context       context;
     cl_command_queue commandQueue;
     cl_mem           clBufferSrc, clBufferDst;
 
-    cl_int hostBuffer[NUM_OF_ELEMENTS] =
-    {
-         0,  1,  2,  3, 
-         4,  5,  6,  7,
-         8,  9, 10, 11, 
-        12, 13, 14, 15,
-        16, 17, 18, 19,
-        20, 21, 22, 23, 
-        24, 25, 26, 27,
-        28, 29, 30, 31, 
-    };
-
-    //Setup the OpenCL Platform, 
-    // Get platform and device information
+    // Настраиваем платформу
     cl_platform_id * platforms = NULL;
-    OCL_CREATE_PLATFORMS( platforms );
+    OCL_CREATE_PLATFORMS(platforms);
 
-    // Get the devices list and choose the type of device you want to run on
-    cl_device_id *device_list = NULL;
+    // Получаем список устройств нужного типа
+    cl_device_id* device_list = NULL;
     OCL_CREATE_DEVICE( platforms[0], DEVICE_TYPE, device_list);
     
-    //Create an execution context for the selected platform and device. 
-    cl_context_properties cps[3] = 
-    {
+    // Создаем исполнительный контекст
+    cl_context_properties cps[3] = {
         CL_CONTEXT_PLATFORM,
         (cl_context_properties)platforms[0],
         0
     };
-    context = clCreateContextFromType(
-        cps,
-        DEVICE_TYPE,
-        NULL,
-        NULL,
-        &status);
+    context = clCreateContextFromType(cps, DEVICE_TYPE, NULL, NULL, &status);
     LOG_OCL_ERROR(status, "clCreateContextFromType Failed..." );
 
-    // Create command queue
-    commandQueue = clCreateCommandQueue(context,
-                                        device_list[0],
-                                        0,
-                                        &status);
+    // Создаем командную очередь
+    commandQueue = clCreateCommandQueue(context, device_list[0], 0, &status);
     LOG_OCL_ERROR(status, "clCreateCommandQueue Failed..." );
 
-    //Create OpenCL device input buffer
-    clBufferSrc = clCreateBuffer(
-        context,
-        CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-        sizeof(cl_uint) * NUM_OF_ELEMENTS,
-        hostBuffer,
-        &status); 
+    // Создаем входной буффер сразу с копированием данных
+    cl_int hostBuffer[NUM_OF_ELEMENTS] =
+    {
+        0,  1,  2,  3,
+        4,  5,  6,  7,
+        8,  9, 10, 11,
+        12, 13, 14, 15,
+        16, 17, 18, 19,
+        20, 21, 22, 23,
+        24, 25, 26, 27,
+        28, 29, 30, 31,
+    };
+    clBufferSrc = clCreateBuffer( context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_uint) * NUM_OF_ELEMENTS, hostBuffer, &status);
     LOG_OCL_ERROR(status, "clCreateBuffer Failed..." );
 
-    //Copy commands
+    // Создаем выходной буффер
     cl_uint copyBuffer[NUM_OF_ELEMENTS] = 
     {
         0,  0,  0,  0, 
@@ -81,20 +64,15 @@ int main(int argc, char *argv[])
         0,  0,  0,  0,
         0,  0,  0,  0
     };
-    
-    clBufferDst = clCreateBuffer(
-        context,
-        CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-        sizeof(cl_uint) * NUM_OF_ELEMENTS,
-        copyBuffer,
-        &status); 
+    clBufferDst = clCreateBuffer( context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_uint) * NUM_OF_ELEMENTS, copyBuffer, &status);
     LOG_OCL_ERROR(status, "clCreateBuffer Failed..." );
 
     //Copy the contents of the rectangular buffer pointed by clBufferSrc to  clBufferSrc
+    // Копируем содержимое квадратного буффера, указываем регионы копирования
     size_t   src_origin[3] = {0, 4, 0};
     size_t   dst_origin[3] = {0, 1, 0};
-    size_t   region[3]     = {3*sizeof(int), 2, 1}; /*width in bytes, height in rows, depth in slices 
-                                                      For 2D depth should be 1*/
+    size_t   region[3]     = {3*sizeof(int), 2, 1}; // Ширина в байтах, высота в строках, глубина в слайсах массивов
+    
     cl_event copyEvent;
     status = clEnqueueCopyBufferRect ( commandQueue, 	//copy command will be queued
                     clBufferSrc,		
--- OpenCL_Books_CodeTest/RavishekharBanger/Code/chapter-3/histogram.cpp
+++ OpenCL_Books_CodeTest/RavishekharBanger/Code/chapter-3/histogram.cpp
@@ -3,76 +3,82 @@
 #include <string.h>
 #include <stdio.h>
 #ifdef __APPLE__
-#include <OpenCL/cl.h>
+    #include <OpenCL/cl.h>
 #else
-#include <CL/cl.h>
+    #include <CL/cl.h>
 #endif
 #include <ocl_macros.h>
 #include <bmp_image.h>
+
 #define USE_HOST_MEMORY
+#define STRINGIFY_TO_TEXT(TEXT) (#TEXT)
 
-const char *histogram_kernel =
-"#define BIN_SIZE 256                                                                  \n"
-"#pragma OPENCL EXTENSION cl_khr_byte_addressable_store : enable                       \n"
-"__kernel                                                                              \n"
-"void histogram_kernel(__global const uint* data,                                      \n"
-"                  __local uchar* sharedArray,                                         \n"
-"                  __global uint* binResultR,                                          \n"
-"                  __global uint* binResultG,                                          \n"
-"                  __global uint* binResultB)                                          \n"
-"{                                                                                     \n"
-"    size_t localId = get_local_id(0);                                                 \n"
-"    size_t globalId = get_global_id(0);                                               \n"
-"    size_t groupId = get_group_id(0);                                                 \n"
-"    size_t groupSize = get_local_size(0);                                             \n"
-"     __local uchar* sharedArrayR = sharedArray;                                       \n"
-"     __local uchar* sharedArrayG = sharedArray + groupSize * BIN_SIZE;                \n"
-"     __local uchar* sharedArrayB = sharedArray + 2 * groupSize * BIN_SIZE;            \n"
-"                                                                                      \n"
-"    /* initialize shared array to zero */                                             \n"
-"    for(int i = 0; i < BIN_SIZE; ++i)                                                 \n"
-"    {                                                                                 \n"
-"        sharedArrayR[localId * BIN_SIZE + i] = 0;                                     \n"
-"        sharedArrayG[localId * BIN_SIZE + i] = 0;                                     \n"
-"        sharedArrayB[localId * BIN_SIZE + i] = 0;                                     \n"
-"    }                                                                                 \n"
-"                                                                                      \n"
-"    barrier(CLK_LOCAL_MEM_FENCE);                                                     \n"
-"                                                                                      \n"
-"    /* calculate thread-histograms */                                                 \n"
-"    for(int i = 0; i < BIN_SIZE; ++i)                                                 \n"
-"    {                                                                                 \n"
-"        uint value = data[globalId * BIN_SIZE + i];                                   \n"
-"        uint valueR = value & 0xFF;                                                   \n"
-"        uint valueG = (value & 0xFF00) >> 8;                                          \n"
-"        uint valueB = (value & 0xFF0000) >> 16;                                       \n"
-"        sharedArrayR[localId * BIN_SIZE + valueR]++;                                  \n"
-"        sharedArrayG[localId * BIN_SIZE + valueG]++;                                  \n"
-"        sharedArrayB[localId * BIN_SIZE + valueB]++;                                  \n"
-"    }                                                                                 \n"
-"                                                                                      \n"
-"    barrier(CLK_LOCAL_MEM_FENCE);                                                     \n"
-"                                                                                      \n"
-"    /* merge all thread-histograms into block-histogram */                            \n"
-"    for(int i = 0; i < BIN_SIZE / groupSize; ++i)                                     \n"
-"    {                                                                                 \n"
-"        uint binCountR = 0;                                                           \n"
-"        uint binCountG = 0;                                                           \n"
-"        uint binCountB = 0;                                                           \n"
-"        for(int j = 0; j < groupSize; ++j)                                            \n"
-"        {                                                                             \n"
-"            binCountR += sharedArrayR[j * BIN_SIZE + i * groupSize + localId];        \n"
-"            binCountG += sharedArrayG[j * BIN_SIZE + i * groupSize + localId];        \n"
-"            binCountB += sharedArrayB[j * BIN_SIZE + i * groupSize + localId];        \n"
-"        }                                                                             \n"
-"                                                                                      \n"
-"        binResultR[groupId * BIN_SIZE + i * groupSize + localId] = binCountR;         \n"
-"        binResultG[groupId * BIN_SIZE + i * groupSize + localId] = binCountG;         \n"
-"        binResultB[groupId * BIN_SIZE + i * groupSize + localId] = binCountB;         \n"
-"    }                                                                                 \n"
-"}                                                                                     \n"
-"                                                                                      \n";
-
+const char *histogram_kernel = STRINGIFY_TO_TEXT(
+    const int BIN_SIZE = 256;
+    //#pragma OPENCL EXTENSION cl_khr_byte_addressable_store : enable // TODO: НЕ хочет компилиться?
+     __kernel void histogram_kernel(__global const uint* data,
+                           __local uchar* sharedArray,
+                           __global uint* binResultR,
+                           __global uint* binResultG,
+                           __global uint* binResultB)
+    {
+        // Получаем данные о текущем месте исполнения в сетке
+        size_t localId = get_local_id(0); // Индекс в пределах группы
+        size_t globalId = get_global_id(0); // Глобальный индекс
+        size_t groupId = get_group_id(0); // Номер группы
+        size_t groupSize = get_local_size(0); // Размер группы
+        
+        // Локальная память очень быстрая, аналог кеша процессора,
+        // Распределяем как нам нужно этот буффер
+        __local uchar* sharedArrayR = sharedArray;
+        __local uchar* sharedArrayG = sharedArray + groupSize * BIN_SIZE;
+        __local uchar* sharedArrayB = sharedArray + 2 * groupSize * BIN_SIZE;
+        
+        // Заполняем нулями наш кеш
+        for(int i = 0; i < BIN_SIZE; ++i){
+            sharedArrayR[localId * BIN_SIZE + i] = 0;
+            sharedArrayG[localId * BIN_SIZE + i] = 0;
+            sharedArrayB[localId * BIN_SIZE + i] = 0;
+        }
+        
+        // Здесь все потоки дожидаются завершения заполнения нулями локальной памяти
+        barrier(CLK_LOCAL_MEM_FENCE);
+        
+        // Вычисляем гистограмму
+        for(int i = 0; i < BIN_SIZE; ++i){
+            // Получаем пиксель
+            uint value = data[globalId * BIN_SIZE + i];
+            // Получаем компоненты пикселя
+            uint valueR = value & 0xFF;
+            uint valueG = (value & 0xFF00) >> 8;
+            uint valueB = (value & 0xFF0000) >> 16;
+            // Для полученной градации цвета увеличиваем значение у локальной переменной
+            sharedArrayR[localId * BIN_SIZE + valueR]++;
+            sharedArrayG[localId * BIN_SIZE + valueG]++;
+            sharedArrayB[localId * BIN_SIZE + valueB]++;
+        }
+        
+        // Дожидаемся завершения
+        barrier(CLK_LOCAL_MEM_FENCE);
+        
+        // Сливаем гистограмы каждого потока в общее значение
+        for(int i = 0; i < BIN_SIZE / groupSize; ++i) {
+            uint binCountR = 0;
+            uint binCountG = 0;
+            uint binCountB = 0;
+            for(int j = 0; j < groupSize; ++j) {
+                binCountR += sharedArrayR[j * BIN_SIZE + i * groupSize + localId];
+                binCountG += sharedArrayG[j * BIN_SIZE + i * groupSize + localId];
+                binCountB += sharedArrayB[j * BIN_SIZE + i * groupSize + localId];
+            }
+            
+            // Сохраняем значение в большие буфферы
+            binResultR[groupId * BIN_SIZE + i * groupSize + localId] = binCountR;
+            binResultG[groupId * BIN_SIZE + i * groupSize + localId] = binCountG;
+            binResultB[groupId * BIN_SIZE + i * groupSize + localId] = binCountB;
+        }
+    }
+);
 
 
 int main(int argc, char *argv[])
@@ -90,12 +96,12 @@ int main(int argc, char *argv[])
     cl_mem     intermediateHistR, intermediateHistG, intermediateHistB; /*Intermediate Image Histogram buffer*/
     cl_uint *  midDeviceBinR, *midDeviceBinG, *midDeviceBinB;
     cl_uint  *deviceBinR,*deviceBinG,*deviceBinB;
-    //Read a BMP Image
-    Image *image;
+    
+    // Читаем BMP картинку и выделяем буфферы
+    Image* image = nullptr;
     std::string filename = "sample_color.bmp";
     ReadBMPImage(filename, &image);
-    if(image == NULL)
-    {
+    if(image == NULL){
         printf("File %s not present...\n", filename.c_str());
         return 0;
     }
@@ -107,16 +113,15 @@ int main(int argc, char *argv[])
     deviceBinG    = (cl_uint*)malloc(binSize * sizeof(cl_uint));
     deviceBinB    = (cl_uint*)malloc(binSize * sizeof(cl_uint));
     
-    //Setup the OpenCL Platform, 
-    //Get the first available platform. Use it as the default platform
+    // Получаем платформу
     status = clGetPlatformIDs(1, &platform, NULL);
     LOG_OCL_ERROR(status, "clGetPlatformIDs Failed." );
 
-    //Get the first available device 
+    // Получаем девайс нужного типа
     status = clGetDeviceIDs (platform, dType, 1, &device, NULL);
     LOG_OCL_ERROR(status, "clGetDeviceIDs Failed." );
     
-    //Create an execution context for the selected platform and device. 
+    // Создаем вычислительный контекст
     cl_context_properties cps[3] = 
     {
         CL_CONTEXT_PLATFORM,
@@ -131,14 +136,15 @@ int main(int argc, char *argv[])
         &status);
     LOG_OCL_ERROR(status, "clCreateContextFromType Failed." );
 
-    // Create command queue
+    // Создаем командную очередь
     commandQueue = clCreateCommandQueue(context,
                                         device,
                                         0,
                                         &status);
     LOG_OCL_ERROR(status, "clCreateCommandQueue Failed." );
+    
 #if !defined(USE_HOST_MEMORY)
-    //Create OpenCL device input buffer
+    // Создаем входной буффер размером с картинку
     imageBuffer = clCreateBuffer(
         context,
         CL_MEM_READ_ONLY,
@@ -147,7 +153,7 @@ int main(int argc, char *argv[])
         &status); 
     LOG_OCL_ERROR(status, "clCreateBuffer Failed while creating the image buffer." );
 
-    //Set input data 
+    // Копируем данные картинки в буффер асинхронно
     cl_event writeEvt;
     status = clEnqueueWriteBuffer(commandQueue,
                                   imageBuffer,
@@ -160,7 +166,7 @@ int main(int argc, char *argv[])
                                   &writeEvt);
     LOG_OCL_ERROR(status, "clEnqueueWriteBuffer Failed while writing the image data." );
 #else
-    //Create OpenCL device input buffer
+    // Создаем входной буффер размером с картинку сразу копируя данные
     imageBuffer = clCreateBuffer(
         context,
         CL_MEM_READ_ONLY|CL_MEM_USE_HOST_PTR,
@@ -169,10 +175,12 @@ int main(int argc, char *argv[])
         &status); 
     LOG_OCL_ERROR(status, "clCreateBuffer Failed while creating the image buffer." );
 #endif
+    
+    // Ждем завершения копирования
     status = clFinish(commandQueue);
     LOG_OCL_ERROR(status, "clFinish Failed while writing the image data." );
     
-    //Create OpenCL device output buffer
+    // Создаем выходные буфферы
     intermediateHistR = clCreateBuffer(
         context, 
         CL_MEM_WRITE_ONLY,
@@ -197,27 +205,29 @@ int main(int argc, char *argv[])
         &status);
     LOG_OCL_ERROR(status, "clCreateBuffer Failed." );
 
-    // Create a program from the kernel source
+    // Создаем вычислительную программу
     cl_program program = clCreateProgramWithSource(context, 1,
             (const char **)&histogram_kernel, NULL, &status);
     LOG_OCL_ERROR(status, "clCreateProgramWithSource Failed." );
 
-    // Build the program
+    // Копилируем
     status = clBuildProgram(program, 1, &device, NULL, NULL, NULL);
     if(status != CL_SUCCESS)
         LOG_OCL_COMPILER_ERROR(program, device);
 
-    // Create the OpenCL kernel
+    // Создаем вычислительное ядро с нужной функцией
     cl_kernel kernel = clCreateKernel(program, "histogram_kernel", &status);
     LOG_OCL_ERROR(status, "clCreateKernel Failed." );
-    // Set the arguments of the kernel
-    status = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&imageBuffer); 
-    status |= clSetKernelArg(kernel, 1, 3 * groupSize * binSize * sizeof(cl_uchar), NULL); 
+    
+    // Аргументы для ядра
+    status = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&imageBuffer); // Обычный буффер
+    status |= clSetKernelArg(kernel, 1, 3 * groupSize * binSize * sizeof(cl_uchar), NULL);  // Локальный буфер для вычислительной группы с высокой скоростью
     status |= clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*)&intermediateHistR);
     status |= clSetKernelArg(kernel, 3, sizeof(cl_mem), (void*)&intermediateHistG);
     status |= clSetKernelArg(kernel, 4, sizeof(cl_mem), (void*)&intermediateHistB);
     LOG_OCL_ERROR(status, "clSetKernelArg Failed." );
-    // Execute the OpenCL kernel on the list
+    
+    // Выполняем вычислительное OpenCL ядро
     cl_event ndrEvt;
     size_t globalThreads = (image->width * image->height) / (binSize*groupSize) * groupSize;
     size_t localThreads = groupSize;
@@ -236,7 +246,7 @@ int main(int argc, char *argv[])
     status = clFinish(commandQueue);
     LOG_OCL_ERROR(status, "clFinish Failed." );
 
-    //Read the histogram back into the host memory.
+    // Вычитываем гистограмму назад
     memset(deviceBinR, 0, binSize * sizeof(cl_uint));
     memset(deviceBinG, 0, binSize * sizeof(cl_uint));
     memset(deviceBinB, 0, binSize * sizeof(cl_uint));
