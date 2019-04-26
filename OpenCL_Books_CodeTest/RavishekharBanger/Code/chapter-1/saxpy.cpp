8968d25bb56e7c79f5b0210c729d363c429b841c
Pavel Ershov <devnulpavel@gmail.com>
2019-04-26 15:04:50 +0300

    New commentaries

--- OpenCL_Books_CodeTest/RavishekharBanger/Code/chapter-1/saxpy.cpp
+++ OpenCL_Books_CodeTest/RavishekharBanger/Code/chapter-1/saxpy.cpp
@@ -11,22 +11,20 @@
 #include <stdlib.h>
 #include <string.h>
 #ifdef __APPLE__
-#include <OpenCL/cl.h>
+    #include <OpenCL/cl.h>
 #else
-#include <CL/cl.h>
+    #include <CL/cl.h>
 #endif
 #include <ocl_macros.h>
 
-//Common defines 
+// Common defines
 #define VENDOR_NAME "AMD"
-#define DEVICE_TYPE CL_DEVICE_TYPE_GPU
+#define DEVICE_TYPE CL_DEVICE_TYPE_GPU // Использовать будем GPU
 
-#define VECTOR_SIZE 1024
+#define VECTOR_SIZE 1024 // 1024 флоата
 
-//OpenCL kernel which is run for every work item created.
-//The below const char string is compiled by the runtime complier
-//when a program object is created with clCreateProgramWithSource 
-//and built with clBuildProgram.
+
+// Ядро OpenCL которое запускается на GPU для каждого потока
 const char *saxpy_kernel =
 "__kernel                                   \n"
 "void saxpy_kernel(const float alpha,       \n"
@@ -41,107 +39,102 @@ const char *saxpy_kernel =
 
 int main(void) {
 
-    cl_int clStatus; //Keeps track of the error values returned. 
+    cl_int clStatus = 0; //Keeps track of the error values returned. 
 
-    // Get platform and device information
-    cl_platform_id * platforms = NULL;
+    // Информация о платформе и устройстве
+    cl_platform_id* platforms = NULL;
 
-    // Set up the Platform. Take a look at the MACROs used in this file. 
-    // These are defined in common/ocl_macros.h
+    // Создаем платформы, макрос из common/ocl_macros.h
     OCL_CREATE_PLATFORMS( platforms );
 
-    // Get the devices list and choose the type of device you want to run on
-    cl_device_id *device_list = NULL;
+    // Получаем список устройств и выбираем тип устройства на котором будем запускать
+    cl_device_id* device_list = NULL;
     OCL_CREATE_DEVICE( platforms[0], DEVICE_TYPE, device_list);
 
-    // Create OpenCL context for devices in device_list
+    // Создаем OpenCL context для устройств из списка
     cl_context context;
-    cl_context_properties props[3] =
+    /*cl_context_properties props[3] =
     {
         CL_CONTEXT_PLATFORM,
         (cl_context_properties)platforms[0],
         0
-    };
-    // An OpenCL context can be associated to multiple devices, either CPU or GPU
-    // based on the value of DEVICE_TYPE defined above.
+    };*/
+    
+    // OpenCL контекст может быть ассоциирован с множеством устройств, в том числе CPU + GPU,
+    // на основании DEVICE_TYPE переданного выше
     context = clCreateContext( NULL, num_devices, device_list, NULL, NULL, &clStatus);
     LOG_OCL_ERROR(clStatus, "clCreateContext Failed..." );
 
-    // Create a command queue for the first device in device_list
+    // Создаем очередь команд для первого устройства из списка
     cl_command_queue command_queue = clCreateCommandQueue(context, device_list[0], 0, &clStatus);
     LOG_OCL_ERROR(clStatus, "clCreateCommandQueue Failed..." );
 
+    // Выделяем память для массивов A, B and C
     float alpha = 2.0;
-    // Allocate space for vectors A, B and C    
-    float *A = (float*)malloc(sizeof(float)*VECTOR_SIZE);
-    float *B = (float*)malloc(sizeof(float)*VECTOR_SIZE);
-    float *C = (float*)malloc(sizeof(float)*VECTOR_SIZE);
-    for(int i = 0; i < VECTOR_SIZE; i++)
-    {
+    float* A = (float*)malloc(sizeof(float)*VECTOR_SIZE);
+    float* B = (float*)malloc(sizeof(float)*VECTOR_SIZE);
+    float* C = (float*)malloc(sizeof(float)*VECTOR_SIZE);
+    
+    // Заполняем массивы
+    for(int32_t i = 0; i < VECTOR_SIZE; i++) {
         A[i] = (float)i;
         B[i] = (float)(VECTOR_SIZE - i);
         C[i] = 0;
     }
 
-    // Create memory buffers on the device for each vector
-    cl_mem A_clmem = clCreateBuffer(context, CL_MEM_READ_ONLY,
-            VECTOR_SIZE * sizeof(float), NULL, &clStatus);
-    cl_mem B_clmem = clCreateBuffer(context, CL_MEM_READ_ONLY,
-            VECTOR_SIZE * sizeof(float), NULL, &clStatus);
-    cl_mem C_clmem = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
-            VECTOR_SIZE * sizeof(float), NULL, &clStatus);
+    // Создаем буфферы на устройстве
+    cl_mem A_clmem = clCreateBuffer(context, CL_MEM_READ_ONLY, VECTOR_SIZE * sizeof(float), NULL, &clStatus);
+    cl_mem B_clmem = clCreateBuffer(context, CL_MEM_READ_ONLY, VECTOR_SIZE * sizeof(float), NULL, &clStatus);
+    cl_mem C_clmem = clCreateBuffer(context, CL_MEM_WRITE_ONLY, VECTOR_SIZE * sizeof(float), NULL, &clStatus);
 
-    // Copy the Buffer A and B to the device. We do a blocking write to the device buffer.
-    clStatus = clEnqueueWriteBuffer(command_queue, A_clmem, CL_TRUE, 0,
-            VECTOR_SIZE * sizeof(float), A, 0, NULL, NULL);
+    // Ставим в очередь копирование данных на девайс, копирование блокирующее
+    clStatus = clEnqueueWriteBuffer(command_queue, A_clmem, CL_TRUE, 0, VECTOR_SIZE * sizeof(float), A, 0, NULL, NULL);
     LOG_OCL_ERROR(clStatus, "clEnqueueWriteBuffer Failed..." );
-    clStatus = clEnqueueWriteBuffer(command_queue, B_clmem, CL_TRUE, 0,
-            VECTOR_SIZE * sizeof(float), B, 0, NULL, NULL);
+    clStatus = clEnqueueWriteBuffer(command_queue, B_clmem, CL_TRUE, 0, VECTOR_SIZE * sizeof(float), B, 0, NULL, NULL);
     LOG_OCL_ERROR(clStatus, "clEnqueueWriteBuffer Failed..." );
 
-    // Create a program from the kernel source
-    cl_program program = clCreateProgramWithSource(context, 1,
-            (const char **)&saxpy_kernel, NULL, &clStatus);
+    // Создаем программу из вычислительного ядра
+    cl_program program = clCreateProgramWithSource(context, 1, (const char **)&saxpy_kernel, NULL, &clStatus);
     LOG_OCL_ERROR(clStatus, "clCreateProgramWithSource Failed..." );
 
-    // Build the program
+    // Компилим программу для GPU
     clStatus = clBuildProgram(program, 1, device_list, NULL, NULL, NULL);
-    if(clStatus != CL_SUCCESS)
+    if(clStatus != CL_SUCCESS){
         LOG_OCL_COMPILER_ERROR(program, device_list[0]);
+    }
 
-    // Create the OpenCL kernel
+    // Создаем вычислительное ядро OpenCL
     cl_kernel kernel = clCreateKernel(program, "saxpy_kernel", &clStatus);
 
-    // Set the arguments of the kernel. Take a look at the kernel definition in saxpy_kernel 
-    // variable. First parameter is a constant and the other three are buffers.
+    // Выставляем аргументы для ядра, обратите внимание на описание в коде ядра -
+    // первый параметр контанта, остальные - буфферы
     clStatus = clSetKernelArg(kernel, 0, sizeof(float),  (void *)&alpha);
     clStatus |= clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&A_clmem);
     clStatus |= clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&B_clmem);
     clStatus |= clSetKernelArg(kernel, 3, sizeof(cl_mem), (void *)&C_clmem);
     LOG_OCL_ERROR(clStatus, "clSetKernelArg Failed..." );
 
-    // Execute the OpenCL kernel on the list
-    size_t global_size = VECTOR_SIZE; // Process one vector element in each work item
-    size_t local_size = 64;           // Process in work groups of size 64.
+    // Ставим в очередь исполнение ядра
+    size_t global_size = VECTOR_SIZE; // Общее количество элементов
+    size_t local_size = 64;           // TODO: Количество элементов в пределах тредгруппы, какое число тут лучше выбирать??
     cl_event saxpy_event;
-    clStatus = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL,
-            &global_size, &local_size, 0, NULL, &saxpy_event);
+    clStatus = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, &global_size, &local_size, 0, NULL, &saxpy_event);
     LOG_OCL_ERROR(clStatus, "clEnqueueNDRangeKernel Failed..." );
 
-    // Read the memory buffer C_clmem on the device to the host allocated buffer C
-    // This task is invoked only after the completion of the event saxpy_event
-    clStatus = clEnqueueReadBuffer(command_queue, C_clmem, CL_TRUE, 0,
-            VECTOR_SIZE * sizeof(float), C, 1, &saxpy_event, NULL);
+    // Читаем в блокирующем режиме данные из GPU на CPU когда у нас завершится вычисление,
+    // отслеживаем окончание вычисления с помощью евента saxpy_event
+    clStatus = clEnqueueReadBuffer(command_queue, C_clmem, CL_TRUE, 0, VECTOR_SIZE * sizeof(float), C, 1, &saxpy_event, NULL);
     LOG_OCL_ERROR(clStatus, "clEnqueueReadBuffer Failed..." );
 
-    // Clean up and wait for all the comands to complete.
+    // Ждем завершения очереди
     clStatus = clFinish(command_queue);
 
-    // Display the result to the screen
-    for(int i = 0; i < VECTOR_SIZE; i++)
+    // Отображаем результат
+    for(int32_t i = 0; i < VECTOR_SIZE; i++){
         printf("%f * %f + %f = %f\n", alpha, A[i], B[i], C[i]);
+    }
 
-    // Finally release all OpenCL objects and release the host buffers.
+    // Уничтожаем данные
     clStatus = clReleaseKernel(kernel);
     clStatus = clReleaseProgram(program);
     clStatus = clReleaseMemObject(A_clmem);
